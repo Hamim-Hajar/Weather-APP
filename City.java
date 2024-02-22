606d03f73f2df2c2bb486b2b333f211abf58e2fc
Hamim Hajar <105655506+Hamim-Hajar@users.noreply.github.com>
2024-02-22 11:48:15 +0100

    Finaliser la classe city et le main

--- City.java
+++ City.java
@@ -1,3 +1,8 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class City {
 private int cityId;
 private String cityName;
@@ -62,5 +67,68 @@ private String cityName;
                 ", currentHumidity=" + currentHumidity +
                 ", currentWindSpeed=" + currentWindSpeed +
                 '}';
+    }
+    private static final String URL = "jdbc:mysql://localhost:3306/weather";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "jujuRO55";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    public static List<City> infocities() throws SQLException {
+        List<City> infocity = new ArrayList<>();
+        String sql = "SELECT * FROM city";
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            int ID = resultSet.getInt("ID");
+            String cityName = resultSet.getString("cityName");
+            int currentTemperature = resultSet.getInt("currentTemperature");
+            int currentHumidity = resultSet.getInt("currentHumidity");
+            int currentWindSpeed = resultSet.getInt("currentWindSpeed");
+            infocity.add(new City(ID, cityName, currentTemperature,currentHumidity,currentWindSpeed));
+        }
+        connection.close();
+        statement.close();
+        resultSet.close();
+        return infocity;
+    }
+    public static void addcity(City city) throws SQLException {
+        String sql = "INSERT INTO city (ID, cityName, currentTemperature,currentHumidity,currentWindSpeed) VALUES (?, ?, ?, ?, ?)";
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1,city.getCityId());
+        statement.setString(2, city.getCityName());
+        statement.setInt(3,city.getCurrentTemperature());
+        statement.setInt(4,city.getCurrentHumidity());
+        statement.setInt(5,city.getCurrentWindSpeed());
+        statement.executeUpdate();
+        connection.close();
+        statement.close();
+        System.out.println("Student added successfully!");
+    }
+    public static void updateCity(City city) throws SQLException {
+        String sql = "UPDATE city SET CityName = ?,CurrentTemperature = ?,CurrentHumidity = ?,CurrentWindSpeed = ? WHERE ID = ?";
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, city.getCityName());
+        statement.setInt(3,city.getCurrentTemperature());
+        statement.setInt(4,city.getCurrentHumidity());
+        statement.setInt(5,city.getCurrentWindSpeed());
+        statement.executeUpdate();
+        connection.close();
+        statement.close();
+        System.out.println("Student updated successfully!");
+    }
+    public static void deleteCity(int id) throws SQLException {
+        String sql = "DELETE FROM city WHERE ID = ?";
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+        connection.close();
+        statement.close();
+        System.out.println("city deleted successfully!");
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,65 @@
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("Hello world!");
-        System.out.println(Databasesaver.infocities());
+        System.out.println(City.infocities());
+
+        int choice, idCity,currentHumidity,currentTemperature,currentWindSpeed;
+        String Cityname ;
+        do {
+            System.out.println("\t\t\t||=======================================||");
+            System.out.println("\t\t\t||                 MENU                  ||");
+            System.out.println("\t\t\t||=======================================||");
+            System.out.println("\t\t\t||              1: Add City              ||");
+            System.out.println("\t\t\t||                2: Update City         ||");
+            System.out.println("\t\t\t||                3: Delete City         ||");
+            System.out.println("\t\t\t||                4: Display All City    ||");
+            System.out.println("\t\t\t||                5: Quitter application ||");
+            System.out.println("\t\t\t||=======================================||");
+            System.out.println("Enter votre choix: ");
+            choice = new Scanner(System.in).nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the ID of the City : ");
+                    idCity = new Scanner(System.in).nextInt();
+                    System.out.print("Donner le nom de la ville : ");
+                    Cityname = new Scanner(System.in).nextLine();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentHumidity = new Scanner(System.in).nextInt();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentTemperature = new Scanner(System.in).nextInt();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentWindSpeed = new Scanner(System.in).nextInt();
+                    City.addcity(new City(idCity, Cityname, currentTemperature,currentHumidity,currentWindSpeed));
+                    break;
+                case 2:
+                    System.out.print("Enter the ID of the City : ");
+                    idCity = new Scanner(System.in).nextInt();
+                    System.out.print("Donner le nom de la ville : ");
+                    Cityname = new Scanner(System.in).nextLine();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentHumidity = new Scanner(System.in).nextInt();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentTemperature = new Scanner(System.in).nextInt();
+                    System.out.print("Donner l'humiditer actuell : ");
+                    currentWindSpeed = new Scanner(System.in).nextInt();
+                    City.updateCity(new City(idCity, Cityname, currentTemperature,currentHumidity,currentWindSpeed));
+                    break;
+                case 3:
+                    System.out.print("Enter City ID to delete: ");
+                    idCity = new Scanner(System.in).nextInt();
+                    City.deleteCity(idCity);
+                    break;
+                case 4:
+                    System.out.println("All students:");
+                    for (City ville : City.infocities()) {
+                        System.out.println(ville);
+                    }
+                    break;
+            }
+        }while (choice != 5) ;
+    }
     }
 }
